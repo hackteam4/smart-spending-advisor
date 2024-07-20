9aac5c1e066226fb90b5cd3f2676f83d518e7a63
juliannaventer <juliannaventer@gmail.com>
2024-07-20 10:44:01 +0200

    Update navigation.component.ts

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { ProfileComponent } from '../../pages/profile/profile.component';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [DashboardComponent, ProfileComponent],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.scss',
 })
-export class NavigationComponent {
-
-}
+export class NavigationComponent {}
