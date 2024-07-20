ed7089b42aaf447cdad2495c90b7741dc7e825a6
Musa-Mabasa <musa14mabasa@gmail.com>
2024-07-20 12:05:37 +0200

    ✨ Created the UI for charts

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -2,36 +2,57 @@ import { Component, Input, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
 
 @Component({
-  selector: 'app-pie-chart',
+  selector: 'app-bar-chart',
   standalone: true,
   imports: [],
-  templateUrl: './pie-chart.component.html',
-  styleUrl: './pie-chart.component.scss',
+  templateUrl: './bar-chart.component.html',
+  styleUrl: './bar-chart.component.scss',
 })
-export class PieChartComponent implements OnInit {
+export class BarChartComponent implements OnInit {
   chart?: any;
-  @Input() data?: Number[] = [300, 50, 100];
-  @Input() title?: string = 'Performance';
+  @Input() data?: Number[] = [65, 59, 80, 81, 56, 55, 40];
 
   ngOnInit() {
     this.createchart();
   }
 
   createchart() {
-    this.chart = new Chart('PieChart', {
-      type: 'doughnut', //this denotes tha type of chart
+    this.chart = new Chart('BarChart', {
+      type: 'bar', //this denotes tha type of chart
       data: {
         // values on X-Axis
-        // labels: ,
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+        ],
         datasets: [
           {
-            label: 'My First Dataset',
+            label: 'Trends',
             data: this.data,
             backgroundColor: [
-              'rgb(255, 99, 132)',
-              'rgb(54, 162, 235)',
-              'rgb(255, 205, 86)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 205, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(201, 203, 207, 0.2)',
             ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 159, 64)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)',
+            ],
+            borderWidth: 1,
           },
         ],
       },
@@ -69,7 +90,7 @@ export class PieChartComponent implements OnInit {
           },
           title: {
             display: true,
-            text: this.title,
+            text: 'Trends',
             position: 'top',
             font: {
               weight: 500,
