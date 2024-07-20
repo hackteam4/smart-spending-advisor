794510b26cc385ca1e2a8f72a258f920a2190ec0
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2024-07-20 11:37:45 +0200

    Adding TransactionClassificationsMap

--- src/app/models/advisor.d.ts
+++ src/app/models/advisor.d.ts
@@ -34,13 +34,32 @@ export enum ClassificationsLevel2 {
 }
 
 export enum ClassificationsLevel3 {
-  Income_Salary,
-  Income_Rent,
-  Income_Other,
-  Expense_Household,
-  Expense_Personal,
+  Expense_Household_Rent,
+  Expense_Household_Utilities,
+  Expense_Household_Repairs_and_Maintenance,
+  Expense_Personal_Grooming,
+  Expense_Personal_Groceries,
   Asset_Property,
   Asset_Vehicle,
   Liabilties_Homeloan,
   Liabilties_AssetFinance,
 }
+
+export const TransactionClassificationsMap = {
+  [ClassificationsLevel1.Income]: {
+    [ClassificationsLevel2.Income_Salary]: null,
+    [ClassificationsLevel2.Income_Rent]: null,
+    [ClassificationsLevel2.Income_Other]: null,
+  },
+  [ClassificationsLevel1.Expense]: {
+    [ClassificationsLevel2.Expense_Household]: [
+      ClassificationsLevel3.Expense_Household_Rent,
+      ClassificationsLevel3.Expense_Household_Utilities,
+      ClassificationsLevel3.Expense_Household_Repairs_and_Maintenance,
+    ],
+    [ClassificationsLevel2.Expense_Personal]: [
+      ClassificationsLevel3.Expense_Personal_Grooming,
+      ClassificationsLevel3.Expense_Personal_Groceries,
+    ],
+  },
+};
