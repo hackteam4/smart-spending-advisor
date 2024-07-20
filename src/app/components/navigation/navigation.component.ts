2814c282362737ac436fec8e2b7568718b146967
Yané <yvanderwesthuizen@jhb.dvt.co.za>
2024-07-20 14:13:41 +0200

    Updated header/navbar

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { ProfileComponent } from '../../pages/profile/profile.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [DashboardComponent, ProfileComponent, RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ReactiveFormsModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+
+  accountForm = new FormGroup({
+    accountNumber: new FormControl('', [Validators.required]),
+  })
+
+  submit() {
+    if (this.accountForm.valid) {
+       const accountNumber = this.accountForm.controls.accountNumber.value;
+    }
+  }
+}
+
+
