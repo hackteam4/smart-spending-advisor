202da51189eec9a3028eb64779881544eadd9ce7
KSmit <ksmit@dvtsoftware.com>
2024-07-20 14:07:11 +0200

    Account Transactions amend

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -16,16 +16,17 @@ export class AccountsService {
 
   getTransactions(accountNumber: string) {
     this.httpClient.get(`https://coral-app-sat5a.ondigitalocean.app/za/pb/v1/accounts/${accountNumber}/transactions?fromDate=2020-11-01`).pipe(map((res: Transactions) => {
-      const ReqList: IRequest[] = [];
+      const requestList: IRequest[] = [];
       for (const transaction of res.data.transactions) {
 
-        ReqList.push({
+        requestList.push({
           amount: transaction.type === 'DEBIT' ? parseInt(transaction.amount) * -1 : parseInt(transaction.amount),
           transactionDate: transaction.transactionDate,
           description: transaction.description,
           balance: parseInt(transaction.runningBalance)
         });
       }
+      return requestList
     }))
   }
 }
