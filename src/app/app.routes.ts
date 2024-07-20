339da72e66877ebc5377492dbbb577e61c5c8d07
Yané <yvanderwesthuizen@jhb.dvt.co.za>
2024-07-20 10:43:19 +0200

    Configure routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
+import {NavigationComponent} from "./components/navigation/navigation.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: NavigationComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      { path: 'profile',
+        component: ProfileComponent
+      },
+    ],
+  },
+  { path: '**',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
+];
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
