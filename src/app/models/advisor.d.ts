9908d64d9847712aeb7035b65ad46aed033751b1
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2024-07-20 12:02:06 +0200

    Adding ClassifiedTransactions interface

--- src/app/models/advisor.d.ts
+++ src/app/models/advisor.d.ts
@@ -4,24 +4,33 @@ export interface PieChart {
   data: Number[];
 }
 
-export interface ClassifiedTransaction {
+// ******************************************************
+// For BE / AI
+// ******************************************************
+
+export interface AIClassifiedTransaction {
   date: Date;
-  detail1: string;
-  detail2: string;
-  classfication_level1: ClassificationsLevel1;
-  classfication_level2: ClassificationsLevel2;
-  classfication_level3: ClassificationsLevel3;
+  detail1: string; // Orgianl detail from statement
+  detail2: string; // If needed
+  classfication: AIClassfication;
   amount: number; // Neg is outflow, pos is inflow
 }
 
-export enum ClassificationsLevel1 {
+export enum AIClassfication {}
+// To be completed
+
+// ******************************************************
+// For FE
+// ******************************************************
+
+export enum FEClassificationsLevel1 {
   Income,
   Expense,
   Asset,
   Liabilties,
 }
 
-export enum ClassificationsLevel2 {
+export enum FEClassificationsLevel2 {
   Income_Salary,
   Income_Rent,
   Income_Other,
@@ -33,7 +42,7 @@ export enum ClassificationsLevel2 {
   Liabilties_AssetFinance,
 }
 
-export enum ClassificationsLevel3 {
+export enum FEClassificationsLevel3 {
   Expense_Household_Rent,
   Expense_Household_Utilities,
   Expense_Household_Repairs_and_Maintenance,
@@ -45,21 +54,21 @@ export enum ClassificationsLevel3 {
   Liabilties_AssetFinance,
 }
 
-export const TransactionClassificationsMap = {
+export interface ClassifiedTransactions {
   [ClassificationsLevel1.Income]: {
-    [ClassificationsLevel2.Income_Salary]: null,
-    [ClassificationsLevel2.Income_Rent]: null,
-    [ClassificationsLevel2.Income_Other]: null,
-  },
-  [ClassificationsLevel1.Expense]: {
-    [ClassificationsLevel2.Expense_Household]: [
-      ClassificationsLevel3.Expense_Household_Rent,
-      ClassificationsLevel3.Expense_Household_Utilities,
-      ClassificationsLevel3.Expense_Household_Repairs_and_Maintenance,
-    ],
-    [ClassificationsLevel2.Expense_Personal]: [
-      ClassificationsLevel3.Expense_Personal_Grooming,
-      ClassificationsLevel3.Expense_Personal_Groceries,
-    ],
-  },
+    [ClassificationsLevel2.Income_Salary]: AIClassifiedTransaction[];
+    [ClassificationsLevel2.Income_Rent]: AIClassifiedTransaction[];
+    [ClassificationsLevel2.Income_Other]: AIClassifiedTransaction[];
+  };
+    [ClassificationsLevel1.Expense]: {
+        [ClassificationsLevel2.Expense_Household]: {
+            [ClassificationsLevel3.Expense_Household_Rent]: AIClassifiedTransaction[];
+            [ClassificationsLevel3.Expense_Household_Utilities]: AIClassifiedTransaction[];
+            [ClassificationsLevel3.Expense_Household_Repairs_and_Maintenance]: AIClassifiedTransaction[];
+        };
+        [ClassificationsLevel2.Expense_Personal]: {};
+        [ClassificationsLevel3.Expense_Personal_Grooming]: AIClassifiedTransaction[];
+        [ClassificationsLevel3.Expense_Personal_Groceries]: AIClassifiedTransaction[];
+    };
+  };
 };
