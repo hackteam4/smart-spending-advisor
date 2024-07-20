227d0c7c6924403e7f99d9d72ca286713732af67
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-07-20 14:06:29 +0200

    Now able to serve

--- src/app/mocks/AIClassifiedTransaction.ts
+++ src/app/mocks/AIClassifiedTransaction.ts
@@ -1,11 +1,11 @@
-import { AIClassfication, TransactionItem } from '../models/advisor';
+import { TransactionItems } from '../models/advisor';
 
 export const mockAIClassifiedTransaction = [
   {
     date: new Date(),
     detail1: 'A transation',
     detail2: 'More details',
-    classfication: TransactionItem.Expense_Personal_Groceries,
+    classfication: TransactionItems.Expense_Personal_Groceries,
     amount: 100,
   },
 ];
