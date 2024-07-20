bdbc921a954f187ebad080ac3f192805ef162992
Yané <yvanderwesthuizen@jhb.dvt.co.za>
2024-07-20 12:11:31 +0200

    Implement profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  profileForm = new FormGroup({
+    totalIncome : new FormControl(0),
+    totalExpenses : new FormControl(0),
+    salary: new FormControl(0),
+    rentIncome: new FormControl(0),
+    householdExpenses: new FormControl(0),
+    personalExpenses: new FormControl(0)
+  })
 
+  calculateTotalIncome() {
+
+  }
+
+  calculateTotalExpenses() {
+
+  }
 }
