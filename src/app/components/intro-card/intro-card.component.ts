47b16c98b8b6778d5f1dddcf66d0eb8906a1a552
juliannaventer <juliannaventer@gmail.com>
2024-07-20 13:42:28 +0200

    cards

---
+++ src/app/components/intro-card/intro-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './intro-card.component.html',
+  styleUrl: './intro-card.component.scss'
+})
+export class IntroCardComponent {
+
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { NavigationComponent } from '../../components/navigation/navigation.component';
 import { RouterOutlet } from '@angular/router';
-import { PieChartComponent } from "../../components/pie-chart/pie-chart.component";
-import { BarChartComponent } from "../../components/bar-chart/bar-chart.component";
-import { PastChartComponent } from "../../components/past-chart/past-chart.component";
+import { BarChartComponent } from '../../components/bar-chart/bar-chart.component';
+import { IntroCardComponent } from '../../components/intro-card/intro-card.component';
+import { NavigationComponent } from '../../components/navigation/navigation.component';
+import { PastChartComponent } from '../../components/past-chart/past-chart.component';
+import { PieChartComponent } from '../../components/pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavigationComponent, RouterOutlet, PieChartComponent, BarChartComponent, PastChartComponent],
+  imports: [
+    NavigationComponent,
+    RouterOutlet,
+    PieChartComponent,
+    BarChartComponent,
+    PastChartComponent,
+    IntroCardComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
